b4e9cf3675771043d3569355352835f6f2f23036
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-04-19 20:25:03 +0700

    add register & login on controller

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,10 +1,36 @@
+const { buildPoseidon } = require("circomlibjs")
 const snarkjs = require("snarkjs")
+const { MerkleTree } = require("fixed-merkle-tree")
+const fs = require("fs")
+const path = require("path")
+
 const { colUser } = require("../../db/firebase")
+
+const vKey = JSON.parse(fs.readFileSync(path.join(__dirname, "../../zk/verification-key.json")))
+
+async function hashSecret(secret) {
+  const poseidon = await circomlib.buildPoseidon()
+  const hash = poseidon([BigInt(secret)])
+  return poseidon.F.toString(hash)
+}
 
 module.exports = {
     register: async(req, res) => {
         try {
-            const { secret } = req.body;
+            const { secret } = req.body
+
+            // hash jadi leaf
+            const leaf = await hashSecret(secret)
+
+            leaves.push(leaf)
+            tree = new MerkleTree(2, leaves)
+
+            res.json({
+            success: true,
+            leaf,
+            index: leaves.length - 1,
+            root: tree.root
+            })
         } catch (error) {
             res.status(500).json({
                 status: false, 
@@ -14,12 +40,25 @@ module.exports = {
     },
     login: async(req, res) => {
         try {
-            
-        } catch (error) {
-            res.status(500).json({
-                status: false, 
-                message: "Internal Server Error!" 
+            const { proof, publicSignals } = req.body
+
+            const verified = await snarkjs.groth16.verify(
+                vKey,
+                publicSignals,
+                proof
+            )
+
+            if (!verified) {
+                return res.json({ success: false })
+            }
+
+            res.json({
+                success: true,
+                message: "ZK proof valid, user is in Merkle Tree"
             })
+
+        } catch (err) {
+            res.status(500).json({ success: false, message: "Internal Server Error!" })
         }
     }
 }
